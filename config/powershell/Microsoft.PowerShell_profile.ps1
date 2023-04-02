4a870f187d6d3b2563c187728fe5d0c288264810
ghtali <ght.ali@hotmail.com>
2023-04-02 23:39:28 +0200

    add ohmyposh

---
+++ config/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,20 @@
+Import-Module posh-git
+oh-my-posh init pwsh --config "$env:USERPROFILE\AWS-Development-Environment-PC\config\prompt\.oh-my-posh.omp.json" | Invoke-Expression
+
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+    dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
+# Winget completion
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+    [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+    $Local:word = $wordToComplete.Replace('"', '""')
+    $Local:ast = $commandAst.ToString().Replace('"', '""')
+    winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
